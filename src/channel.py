4260bafea2eb56c755bfa8e34a26123a9873b936
Anastasia Chernysheva <nastya770098@gmail.com>
2023-07-27 17:03:20 +0300

    hw1

--- src/channel.py
+++ src/channel.py
@@ -10,11 +10,9 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
 
-
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         api_key: str = os.getenv('API_KEY')
         youtube = build('youtube', 'v3', developerKey=api_key)
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         return print(json.dumps(channel, indent=2, ensure_ascii=False))
-
