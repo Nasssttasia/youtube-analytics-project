77ff8859125d47f6f56496aaaa8d8a99a8d6d61e
Anastasia Chernysheva <nastya770098@gmail.com>
2023-07-30 18:48:52 +0300

    hw2 not done

--- src/channel.py
+++ src/channel.py
@@ -6,9 +6,15 @@ from googleapiclient.discovery import build
 class Channel:
     """Класс для ютуб-канала"""
 
-    def __init__(self, channel_id: str) -> None:
+    def __init__(self, channel_id: str, name, description, link, subscribers, videos, views) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.name = name
+        self.description = description
+        self.link = link
+        self.subscribers = subscribers
+        self.videos = videos
+        self.views = views
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
@@ -16,3 +22,14 @@ class Channel:
         youtube = build('youtube', 'v3', developerKey=api_key)
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         return print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        api_key: str = os.getenv('API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        service = json.dumps(channel, indent=2, ensure_ascii=False)
+        return service
+
+    def to_json(self):
+        pass
