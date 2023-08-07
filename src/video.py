f18edceb79baa00e247f4372d1eedb0ad9e16232
Anastasia Chernysheva <nastya770098@gmail.com>
2023-08-07 23:28:11 +0300

    hw3

--- src/video.py
+++ src/video.py
@@ -18,30 +18,22 @@ class Video:
         youtube = build('youtube', 'v3', developerKey=api_key)
         video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                id=self.video_id
-                                               )
+                                               ).execute()
         return video_response
 
     def __str__(self):
-        return f'{self.video_title}'
+        return self.video_title
 
 
-class PLVideo:
-    def __init__(self, video_id_, pl_id):
-        self.video_id_ = video_id_
+class PLVideo(Video):
+    def __init__(self, video_id, pl_id):
+        super().__init__(video_id)
         self.pl_id = pl_id
-        self.__info_to_print = self.get_info_video().execute()
+        self.__info_to_print = self.get_info_video()
         self.video_title = self.__info_to_print['items'][0]['snippet']['title']
         self.view_count = self.__info_to_print['items'][0]['statistics']['viewCount']
         self.like_count = self.__info_to_print['items'][0]['statistics']['likeCount']
         self.comment_count = self.__info_to_print['items'][0]['statistics']['commentCount']
 
-    def get_info_video(self):
-        api_key: str = os.getenv('API_KEY')
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=self.video_id_
-                                               ).execute()
-        return video_response
-
     def __str__(self):
-        return f'{self.video_title}'
+        return self.video_title
