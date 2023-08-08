7e45011d2251af94e79dee6256131844745eb98e
Anastasia Chernysheva <nastya770098@gmail.com>
2023-08-08 21:52:30 +0300

    hw4

--- src/video.py
+++ src/video.py
@@ -29,11 +29,6 @@ class PLVideo(Video):
     def __init__(self, video_id, pl_id):
         super().__init__(video_id)
         self.pl_id = pl_id
-        self.__info_to_print = self.get_info_video()
-        self.video_title = self.__info_to_print['items'][0]['snippet']['title']
-        self.view_count = self.__info_to_print['items'][0]['statistics']['viewCount']
-        self.like_count = self.__info_to_print['items'][0]['statistics']['likeCount']
-        self.comment_count = self.__info_to_print['items'][0]['statistics']['commentCount']
 
     def __str__(self):
         return self.video_title
