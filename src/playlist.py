b2a1a80356839c05481daaf712c409129a243ef2
Anastasia Chernysheva <nastya770098@gmail.com>
2023-08-12 00:19:35 +0300

    hw5

--- src/playlist.py
+++ src/playlist.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime, timedelta
 
 import isodate
 from googleapiclient.discovery import build
@@ -8,8 +9,8 @@ class PlayList:
     def __init__(self, pl_id):
         self.pl_id = pl_id
         self.__info_to_print = self.get_info_pl()
-        # self.__pl_title = self.__info_to_print['items'][0]['snippet']['title'] как прописать путь?
-        # self.__pl_url = self.__info_to_print['items'][0]['statistics']['viewCount']
+        self.__pl_title = self.__info_to_print['items'][0]['snippet']['title']
+        self.__pl_url = f"https://www.youtube.com/playlist?list={self.__info_to_print['items'][0]['id']}"
 
     @property
     def title(self):
@@ -22,15 +23,10 @@ class PlayList:
     def get_info_pl(self):
         api_key: str = os.getenv('API_KEY')
         youtube = build('youtube', 'v3', developerKey=api_key)
-        pl_response = youtube.playlists().list(channelId=self.pl_id,
-                                     part='contentDetails,snippet',
-                                     maxResults=50,
-                                     ).execute()
+        pl_response = youtube.playlists().list(id=self.pl_id, part='snippet').execute()
         return pl_response
 
-    def total_duration(self):
-        """возвращает объект класса `datetime.timedelta` с суммарной длительность плейлиста
-        (обращение как к свойству, использовать `@property`)"""
+    def get_info_video(self):
         api_key: str = os.getenv('API_KEY')
         youtube = build('youtube', 'v3', developerKey=api_key)
         playlist_videos = youtube.playlistItems().list(playlistId=self.pl_id,
@@ -43,16 +39,22 @@ class PlayList:
         video_response = youtube.videos().list(part='contentDetails,statistics',
                                                id=','.join(video_ids)
                                                ).execute()
-        for video in video_response['items']:
-            # YouTube video duration is in ISO 8601 format
-            iso_8601_duration = video['contentDetails']['duration']
-            duration = isodate.parse_duration(iso_8601_duration)
-            return duration
+        return video_response
 
     @property
     def total_duration(self):
-        return self.total_duration()
+        """возвращает объект класса `datetime.timedelta` с суммарной длительность плейлиста
+        (обращение как к свойству, использовать `@property`)"""
+        video_response = self.get_info_video()
+        duration = isodate.parse_duration("PT0S")
+        for video in video_response['items']:
+            # YouTube video duration is in ISO 8601 format
+            iso_8601_duration = video['contentDetails']['duration']
+            duration += isodate.parse_duration(iso_8601_duration)
+
+        return duration
+
 
     def show_best_video(self):
         """возвращает ссылку на самое популярное видео из плейлиста (по количеству лайков)"""
-        pass
+        return f"https://youtu.be/{self.get_info_video()['items'][3]['id']}"
